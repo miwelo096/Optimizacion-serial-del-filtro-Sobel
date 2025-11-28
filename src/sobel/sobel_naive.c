4967b0195df7864957d9099a09252c3fbbad961a
Miguel Sánchez Illescas <92173061+miwelo096@users.noreply.github.com>
2025-11-28 14:23:05 +0100

    Changed data types for more precission

--- src/sobel/sobel_naive.c
+++ src/sobel/sobel_naive.c
@@ -3,19 +3,19 @@
 #include <string.h>
 #include <math.h>
 
-int** read_image(const char* filename, int rows, int cols) {
+float** read_image(const char* filename, int rows, int cols) {
     FILE* file = fopen(filename, "r");
     if (!file) {
         printf("Error al abrir el archivo %s\n", filename);
         return NULL;
     }
     // Interpretacion naive, crea una matriz para almacenar la imagen y reserva memoria
-    int **matriz;
+    float **matriz;
 
     // Reserva memoria
-    matriz = (int**)malloc(rows * sizeof(int*));
+    matriz = (float**)malloc(rows * sizeof(float*));
     for (int i = 0; i < rows; i++) {
-        matriz[i] = (int*)malloc(cols * sizeof(int));
+        matriz[i] = (float*)malloc(cols * sizeof(float));
     }
 
     // Lee los valores de la imagen desde el archivo
@@ -36,13 +36,14 @@ void free_matrix(int** matriz, int rows) {
     free(matriz);
 }
 
-int** sobel_filter(int** matriz, int rows, int cols) {
-    // Kernel gx y gy del filtro Sobel
+float** sobel_filter(float** matriz, int rows, int cols) {
+    // Kernel gx del filtro Sobel
     int gx[3][3] = {
         {-1, 0, 1},
         {-2, 0, 2},
         {-1, 0, 1}
     };
+    // Kernel gy del filtro Sobel
     int gy[3][3] = {
         {1, 2, 1},
         {0, 0, 0},
@@ -50,23 +51,23 @@ int** sobel_filter(int** matriz, int rows, int cols) {
     };
 
     for (int i = 1; i < rows - 1; i++) {
+        // Bucle que recorre los píxeles de la imagen, excluyendo los bordes
         for (int j = 1; j < cols - 1; j++) {
             int sobelX = 0;
             int sobelY = 0;
-
+            // Bucle que aplica convolución al área 3x3 alrededor del píxel actual para cada uno de los kernels
             for (int u = -1; u <= 1; u++) {
                 for (int v = -1; v <= 1; v++) {
-                    // Pixel al que se aplica el kernel
                     int pixel = matriz[i + u][j + v];
-                    sobelX += pixel * gx[u + 1][v + 1];
-                    sobelY += pixel * gy[u + 1][v + 1];
+                    sobelX = sobelX+ pixel * gx[u + 1][v + 1];
+                    sobelY = sobelY+ pixel * gy[u + 1][v + 1];
                 }
             }
 
-            // Magnitud del gradiente
+            // Magnitud del gradiente después de aplicar los filtros Sobel
             int magnitude = (int)sqrt((sobelX * sobelX) + (sobelY * sobelY));
-            // Limitar el valor a 0-255
-            //if (magnitude < 0) magnitude = 0;
+            // Limita el valor a 0-255
+            if (magnitude < 0) magnitude = 0;
             if (magnitude > 255) magnitude = 255;
             matriz[i][j] = magnitude;
         }
