f71691c9047b68e5d40d1f759aeca32c9fa93b09
Miguel Sánchez Illescas <92173061+miwelo096@users.noreply.github.com>
2025-12-01 22:28:10 +0100

    Prueba básica de sobel naive

--- src/sobel/sobel_naive.c
+++ src/sobel/sobel_naive.c
@@ -93,3 +93,34 @@ int** sobel_filter(int** matriz, int rows, int cols) {
 
     return matriz;
 }
+
+int main() {
+    const char* input_file = "txt/Eiffelturm.txt";
+    const char* output_file = "txt/Eiffelturm_sobel_naive.txt";
+
+    // Introzuzco las dimensiones de la imagen directamente en el programa para evitar entrada/salida o tener que leerlas del archivo
+    int rows = 512;
+    int cols = 512;
+
+    // Almaceno la imagen en una matriz
+    int** imagen = read_image(input_file, rows, cols);
+    if (!imagen) {
+        return 1;
+    }
+
+    // Aplico el filtro Sobel
+    int** imagen_sobel = sobel_filter(imagen, rows, cols);
+    if (!imagen_sobel) {
+        free_matrix(imagen, rows);
+        return 1;
+    }
+
+    // Escribo la imagen procesada
+    write_image(output_file, imagen_sobel, rows, cols);
+
+    // Libero la memoria
+    free_matrix(imagen, rows);
+
+    printf("Filtro Sobel aplicado y guardado en %s\n", output_file);
+    return 0;
+}
